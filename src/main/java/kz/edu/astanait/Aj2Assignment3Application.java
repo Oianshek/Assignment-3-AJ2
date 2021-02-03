aa6517df3eea93468c640dcda3d767b63491d298
oianshek <oiansheks@gmail.com>
2021-02-03 16:17:38 +0500

    First Commit

---
+++ src/main/java/kz/edu/astanait/Aj2Assignment3Application.java
@@ -0,0 +1,13 @@
+package kz.edu.astanait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aj2Assignment3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aj2Assignment3Application.class, args);
+    }
+
+}
---
+++ src/test/java/kz/edu/astanait/Aj2Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.edu.astanait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aj2Assignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
